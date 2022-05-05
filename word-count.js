4b106e93d0c8efe8d3f96d7201e37040ea9468f5
Sara Ibarra <saraiba33@gmail.com>
2022-05-05 12:24:53 -0600

    Add code to separate words

--- word-count.js
+++ word-count.js
@@ -1,1 +1,11 @@
-export function count() {}
+export function count(string) {
+    return string
+        .trim()
+        .replace(/\s+/g, " ")
+        .toLowerCase()
+        .split(" ")
+        .reduce((arrayOfWords, words) => {
+            arrayOfWords[words] = (arrayOfWords[words] || 0) + 1;
+            return arrayOfWords
+        }, {})
+}
